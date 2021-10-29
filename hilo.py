15076e1d5dc2cd34ac8439df0a5964f5dcc8c423
ronaldcoe <ronaldcoello85@gmail.com>
2021-10-28 23:00:52 -0600

    Added new functions to finish program

--- hilo.py
+++ hilo.py
@@ -9,7 +9,7 @@ Points are won or lost based on whether or not the player guessed correctly.
 #Generate the card using randit
 
 def cardGenerator():
-    card = randint(1, 14)
+    card = randint(1, 13)
     return card
 
 #Get the input (Guess) from the user and validate it
@@ -18,7 +18,7 @@ def getInput():
     done = False
     while not done:
         userGuess = (input('Higher or lower?[h/l]: '))
-        if userGuess == "h" or userGuess == "l":
+        if userGuess.lower() == "h" or userGuess.lower() == "l":
             done =  True
         else:
             print('Sorry, that is not a correct input')
@@ -26,11 +26,60 @@ def getInput():
     return userGuess
 
  #Compare the UserGess to the card generated
-def compare(userGuess, card):
-    pass
+def compareCards(userGuess, card1, card2):
+    secondValue = 0
+    if userGuess == 'l':
+        if card1 > card2:
+            secondValue = 100
+        else:
+            secondValue = -75
+    elif userGuess == 'h':
+        if card1 < card2:
+            secondValue = 100
+        else:
+            secondValue = -75
+
+    return secondValue
+
+#do the math add or reduce points
+def computePoints(points, pointsToAddOrReduce):
+    points = points + pointsToAddOrReduce
+    return points
 
 
+    
+#function to check if the user wants and can keep playing
+def canPlay(points):
+    done = False
+    if points <= 0:
+        done = True
+    elif points > 0:
+        userChoice = input('Keep playing?: [y/n]')           
+        if userChoice.lower() == 'y':
+            done = False
+        elif userChoice.lower() == 'n':
+            done = True
 
-def main():
+    return done
 
-    pass
+def main():    
+    done = False
+    points = 300
+    while not done:        
+        card1 = cardGenerator() #generates first card with function cardGenerator()
+        card2 = cardGenerator() #generates second card with function cardGenerator()
+        print(f'The card is {card1}') #Show the first card
+        userGuess = getInput() #call the function getInput(), get the user guess, and store it in a variable
+        print(f'The next card is {card2}') #show the second card
+        pointsToAddOrReduce = compareCards(userGuess, card1, card2) #compare the cards, return a positive or negative value to add or reduce the points, and store it in a variable
+        points = computePoints(points, pointsToAddOrReduce) #do the math and reasign the variable points with the new points
+        #check if point is a negative number and set points to 0
+        if points <= 0:
+            points = 0 
+        print(f'Your score is: {points}') #show new score
+        done = canPlay(points) #check if points are greater than 0 and user wants to keep playing
+             
+    print('Game Over')
+            
+
+main()
